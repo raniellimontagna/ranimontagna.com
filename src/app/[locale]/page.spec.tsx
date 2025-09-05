b02653cdefa4ee5de50850aadd3e4b912da81a85
Ranielli Montagna <raniellimontagna@hotmail.com>
2025-09-05 11:08:08 -0300

    test: enhance page tests with NextIntlClientProvider for locale handling

--- src/app/[locale]/page.spec.tsx
+++ src/app/[locale]/page.spec.tsx
@@ -1,9 +1,18 @@
 import { expect, test } from 'vitest'
 import { render, screen, waitFor } from '@testing-library/react'
-import Page from '../app/page'
+import { NextIntlClientProvider } from 'next-intl'
+
+import Page from './page'
+import pt from '../../../messages/pt.json'
 
 test('Page', () => {
-  render(<Page />)
+  render(<Page />, {
+    wrapper: ({ children }) => (
+      <NextIntlClientProvider locale="pt-BR" messages={pt}>
+        {children}
+      </NextIntlClientProvider>
+    ),
+  })
 
   waitFor(() => {
     expect(screen.getByText('Ranielli Montagna')).toBeDefined()
