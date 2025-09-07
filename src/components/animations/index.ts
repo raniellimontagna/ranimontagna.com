69a5e2bd1edd004710f6f6e1c148137840088675
Ranielli Montagna <raniellimontagna@hotmail.com>
2025-09-07 15:08:10 -0300

    refactor: removes in-view animation component and related exports

--- src/components/animations/index.ts
+++ src/components/animations/index.ts
@@ -1,12 +1,2 @@
 export { StaggerContainer, StaggerItem } from './stagger-container'
 export { FadeIn } from './fade-in'
-
-export {
-  InViewAnimation,
-  fadeInVariants,
-  slideUpVariants,
-  slideLeftVariants,
-  slideRightVariants,
-  scaleVariants,
-  rotateVariants,
-} from './in-view-animation'
